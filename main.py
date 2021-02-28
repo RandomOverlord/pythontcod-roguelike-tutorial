45a836ea87b952e2a35856ffcc3f7346521e2523
RandomOverlord <abhay.fernandes@protonmail.com>
2021-02-28 15:05:17 -0500

    Initial Commit

---
+++ main.py
@@ -0,0 +1,54 @@
+import tcod
+
+from actions import EscapeAction, MovementAction
+from entity import Entity
+from input_handlers import EventHandler
+
+def main() -> None:
+    # Defining size of screen
+    screen_width = 80
+    screen_height = 50
+
+    # Telling tcod what tileset to use
+    tileset = tcod.tileset.load_tilesheet(
+        "Assets\\dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
+    )
+
+    event_handler = EventHandler()
+
+    player = Entity(int(screen_width / 2), int(screen_height / 2), "@", (255, 255, 255))
+    npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), "@", (255, 255, 0))
+    entities = {player, npc}
+
+    #Creating the Screen that is drawn to
+    with tcod.context.new_terminal(
+        screen_width,
+        screen_height,
+        tileset=tileset,
+        title="Yet Another Roguelike Tutorial",
+        vsync=True,
+    ) as context:
+        # Creating the console to host the screen
+        root_console = tcod.Console(screen_width, screen_height, order='F')
+        # Main gameloop
+        while True:
+            root_console.print(x=player.x, y=player.y, string=player.char, fg=player.color)
+            #Updates the console every frame
+            context.present(root_console)
+
+            root_console.clear()
+
+            for event in tcod.event.wait():
+                action = event_handler.dispatch(event)
+
+                if action is None:
+                    continue
+
+                if isinstance(action, MovementAction):
+                    player.move(dx=action.dx, dy=action.dy)
+
+                elif isinstance(action, EscapeAction):
+                    raise SystemExit
+
+if __name__ == "__main__":
+    main()
