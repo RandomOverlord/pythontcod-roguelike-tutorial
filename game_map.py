5ff590c585268b71dd259eb7ba3e9c32d1461b62
RandomOverlord <abhay.fernandes@protonmail.com>
2021-03-14 20:07:18 -0400

    Refactor the Project

--- game_map.py
+++ game_map.py
@@ -7,10 +7,12 @@ from typing import Iterable, Optional, TYPE_CHECKING
 import tile_types
 
 if TYPE_CHECKING:
+    from engine import Engine
     from entity import Entity
 class GameMap:
-    def __init__(self, width: int, height: int, entities: Iterable[Entity] = ()):
+    def __init__(self, engine: Engine, width: int, height: int, entities: Iterable[Entity] = ()):
         self.width, self.height = width, height
+        self.engine = engine
         self.tiles = np.full((width, height), fill_value=tile_types.wall, order="F")
         self.entities = set(entities)
         self.visible = np.full((width, height), fill_value=False, order="F") # Visible tiles in the current state
@@ -30,7 +32,7 @@ class GameMap:
         console.tiles_rgb[0:self.width, 0:self.height] = np.select(
             condlist=[self.visible, self.explored],
             choicelist=[self.tiles["light"], self.tiles["dark"]],
-            default=tile_types.SHROUD
+            default=tile_types.SHROUD,
         )
 
         for entity in self.entities:
