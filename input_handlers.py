f4b9aaae55aacdb1d1caed939b58e37677ffa715
RandomOverlord <abhay.fernandes@protonmail.com>
2021-03-14 22:07:18 -0400

    cleaned up player death

--- input_handlers.py
+++ input_handlers.py
@@ -55,7 +55,7 @@ class EventHandler(tcod.event.EventDispatch[Action]):
         raise SystemExit()
 
 
-class MainGameEvent(tcod.event.EventDispatch[Action]):
+class MainGameEventHandler(EventHandler):
     def __init__(self, engine: Engine) -> None:
         self.engine = engine
     
@@ -70,9 +70,6 @@ class MainGameEvent(tcod.event.EventDispatch[Action]):
 
             self.engine.handle_enemy_turns()
             self.engine.update_fov()
-
-    def ev_quit(self, event: tcod.event.Quit) -> Optional[Action]:
-        raise SystemExit()
 
     def ev_keydown(self, event: tcod.event.KeyDown) -> Optional[Action]:
         action: Optional[Action] = None
@@ -91,3 +88,22 @@ class MainGameEvent(tcod.event.EventDispatch[Action]):
             action = EscapeAction(player)
 
         return action
+
+class GameOverEventHandler(EventHandler):
+    def handle_events(self) -> None:
+        for event in tcod.event.wait():
+            action = self.dispatch(event)
+
+            if action is None:
+                continue
+        
+            action.perform()
+    def ev_keydown(self, event: tcod.event.KeyDown) -> Optional[Action]:
+        action: Optional[Action] = None
+
+        key = event.sym
+
+        if key == tcod.event.K_ESCAPE:
+            action = EscapeAction(self.engine.player)
+        
+        return
